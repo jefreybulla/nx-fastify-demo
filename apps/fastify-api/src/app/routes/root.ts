b79177e47fc5e6bed2a8b93f5d7dad45ac9c8bab
Jefrey Bulla <jefrey.bulla@gmail.com>
2024-10-01 17:48:47 -0400

    add new route

--- apps/fastify-api/src/app/routes/root.ts
+++ apps/fastify-api/src/app/routes/root.ts
@@ -4,4 +4,9 @@ export default async function (fastify: FastifyInstance) {
   fastify.get('/', async function () {
     return { message: 'Hello API' };
   });
+
+
+  fastify.get('/ping', async function () {
+    return { message: 'pong' };
+  });
 }
